d2a4b1406de0e85598039bb429bc8294b5d43781
SadiyaIslamSamiya <samiyasadiya333@gmail.com>
2026-04-27 21:16:38 +0600

    samiya

--- src/components/home/Offer.jsx
+++ src/components/home/Offer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import chatbotUi from '../../assets/images/chatbot_ui.png'
+import { Ri24HoursFill } from 'react-icons/ri'
+import { FaWhatsapp } from 'react-icons/fa'
+import { BsGraphUpArrow } from 'react-icons/bs'
+import { VscRobot } from 'react-icons/vsc'
 const Offer = () => {
   return (
     <>
-      <div className="container px-[5%]">
-        <div className=" grid grid-cols-3 ">
+      <div className="container px-[5%] rounded-2xl my-9 py-5 shadow-[0_0_50px_rgba(245,166,35,0.12)]">
+        <div className=" grid grid-cols-3 content-center ">
+          {/* -----------left side----------- */}
             <div className=" col-span-2">
                 <h5 className='text-[12px] uppercase text-gold font-semibold font-body tracking-[3px] mb-6'>
                     Chapter III
@@ -14,11 +19,40 @@ const Offer = () => {
                     <span className="text-gold"> Paying Clients</span>
                 </h2>
                 <p className='text-muted text-[clamp(1rem,2vw,1.25rem)]'>
-                    We now provide AI Chatbot & WhatsApp Automation services to help your business capture leads, answer customers instantly, and grow 24/7.
+                    Turn your website visitors into paying customers automatically using AI Chatbots & WhatsApp Automation — working 24/7 even when you sleep. Businesses using AI automation are already getting 2–5x more leads than traditional methods.
                 </p>
+                <div className="grid grid-cols-2 grid-rows-2 gap-2 mt-6 w-[50%]">
+                  {/* item-1 */}
+                  <div className="bg-gold/20 rounded-full p-2 inline-flex items-center gap-3 border border-gold text-gold">
+                    <Ri24HoursFill className=' w-5 h-5'/>
+                    <p className=' font-body font-light text-[12px]'>24/7 Auto Reply</p>
+                  </div>
+                  {/* item-2 */}
+                  <div className="bg-gold/20 rounded-full p-2 inline-flex items-center gap-3 border border-gold text-gold">
+                    <FaWhatsapp className=' w-5 h-5'/>
+                    <p className=' font-body font-light text-[12px]'>WhatsApp Integration </p>
+                  </div>
+                  {/* item-3 */}
+                  <div className="bg-gold/20 rounded-full p-2 inline-flex items-center gap-3 border border-gold text-gold">
+                    <BsGraphUpArrow className=' w-5 h-5'/>
+                    <p className=' font-body font-light text-[12px]'>More Leads</p>
+                  </div>
+                  {/* item-4 */}
+                  <div className="bg-gold/20 rounded-full p-2 inline-flex items-center gap-3 border border-gold text-gold">
+                    <VscRobot className=' w-5 h-5'/>
+                    <p className=' font-body font-light text-[12px]'>AI Smart Responses</p>
+                  </div>
+                </div>
             </div>
-            <div className="mx-auto">
-                <img src={chatbotUi} alt="" className='h-[350px]  rounded-2xl'/>
+            {/* ---------------right side--------------- */}
+            <div className="mx-auto relative">
+              <div className="border border-gold rounded-2xl shadow-[0_0_50px_rgba(245,166,35,0.12)] ">
+                <img src={chatbotUi} alt="" className='h-[400px]  rounded-2xl'/>
+              </div>
+              {/* Floating Badge */}
+              <div className="absolute -top-4 -right-10 bg-[#f6a500] text-black text-xs font-bold px-4 py-2 rounded-full shadow-lg animate-bounce">
+                AI Active Now
+              </div>
             </div>
         </div>
         
